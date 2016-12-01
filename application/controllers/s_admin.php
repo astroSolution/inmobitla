874521b29172250dbb1ebbbe466111ddeb02dba0
Jose Gabriel E <j-e_1997@hotmail.es>
2016-12-01 19:53:05 -0400

    cambio importante para listar publicaciones y admin
    Se realizo un nuevo query para listar las publicaciones, debe  buscar
    solo las publicaciones que esten activas y que el usuario de dicha
    publicacion este activo, admin_publicaciones y admin_usuarios Listo!

--- application/controllers/s_admin.php
+++ application/controllers/s_admin.php
@@ -10,6 +10,8 @@ class S_admin extends CI_Controller{
       $this->load->model(array('Publicaciones_model','Usuario_model'));
       if($this->Usuario_model->verificalogin()){
          print "<script>alert('Favor Hacer Login'); window.location.href = \"/inmobitla/seguridad/login/s_admin\";</script>";
+       }elseif(!isset($this->session->datosusu[0]->nivel)){
+          print "<script>alert('Debe ser adminisitrador para usar esta herramienta'); window.location.href = \"/inmobitla\";</script>";
        }
     //Codeigniter : Write Less Do More
   }
@@ -21,10 +23,32 @@ class S_admin extends CI_Controller{
      $this->load->view('secciones/administracion/administracion', $data);
   }
 
-  function admin_usuario(){
+  function admin_usuario($id=0){
+    if($id!=0){
+      $this->Usuario_model->eliminaUsu($id);
+    }
     $data['titulo'] = "Aministracion de Usuario";
     $data['usuarios'] = $this->Usuario_model->cargaUsu();
     $this->load->view('secciones/administracion/admin_usuario',$data);
+  }
+
+  function admin_publicaciones($id=0){
+    if($id!=0){
+      $this->Publicaciones_model->desactivaPub($id);
+    }
+    $data['titulo'] = "Administracion de Publicaciones";
+    $data['publicaciones'] = $this->Usuario_model->publicacionUsuActivo();
+    $this->load->view('secciones/administracion/admin_publicaciones',$data);
+  }
+
+  function admin_tipos($id=0){
+    $data['titulo'] = "Administrador de Tipos";
+    if($id!=0){
+      $data['tipo']=$this->Publicaciones_model->cargaTipos($id);
+    }else{
+      $data['tipos'] = $this->Publicaciones_model->cargaTipos();
+    }
+    $this->load->view('secciones/administracion/admin_tipos',$data);
   }
 
 }
